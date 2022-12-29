aeed508d6c15c910503b1dc647008e23911ad5ec
Katy Rosli <katyrosli@gmail.com>
2022-12-29 20:54:02 +0100

    update Home.tsx
    Update Home.tsx
    - better intro description

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,8 +8,8 @@ export function Home() {
     <main>
       <header className='header'>
         <h1 className='header__title'>Hi,<br/>I'm Katy Rosli.</h1>
-        <h2 className='header__body'>A Frontend Developer, based in Stockholm.</h2>
-        <p className='header__content'>JavaScript  .  TypeScript  .  React  .  NextJs  .  HTML  .  CSS</p>
+        <h2 className='header__body'>A Frontend Developer, with Backend knowledge.</h2>
+        <p className='header__content'>React .  TypeScript  .  JavaScript .  NextJs  . CSS  .  Node.js</p>
         <HashLink to='/#portfolio' scroll={(el: HTMLElement) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}><Button className='button header__button header__button--primary'>View Portfolio</Button></HashLink>
       </header>
       <Portfolio />
