b86cff73f4339a9418a039b4cba2d50d40c43437
Katy Rosli <katyrosli@gmail.com>
2023-03-21 09:39:51 +0100

    added project describtion

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -29,7 +29,7 @@ export function Portfolio() {
       tiny: TinyNextJsGeoGameApp,
       alt: 'Geo Game App',
       title: 'Geo Game App',
-      tags: 'Next Js, React Js, TypeScript, HTML, SCSS, Netlify',
+      tags: 'React Js, TypeScript, HTML, SCSS, Netlify',
       button: 'Visit Github',
       github: 'https://github.com/KatyRosli/game', 
       button2: 'Visit Website',
