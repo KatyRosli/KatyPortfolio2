da5adf6c5e6e81204e5091386abdb2dac5f8a4d0
Katy Rosli <katyrosli@gmail.com>
2023-04-14 18:11:55 +0200

    added msg & remove console log

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -6,22 +6,24 @@ import Button from 'react-bootstrap/Button';
 
 export function Contact() {
   const form = useRef<null | HTMLFormElement>(null);
+  const [message, setMessage] = useState<string | null>(null);
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   
     emailjs.sendForm('service_ljqlf28', 'template_fye02wi', form.current, 't_lIr25-4DnNdT2bQ')
-      .then((result: unknown) => {
-          console.log((result as {text: string;}).text);
-          console.log('message sent');
-      }, (error: unknown) => {
-          console.log((error as {text: string;}).text);
+      .then(() => {
+          setMessage('Thank you for your message! I will get back to you soon.');
+      }, () => {
+          setMessage('Oops, please fill in all of the input fields.');
       });
   };
+
   return (
     <Form className='form' id='contact' ref={form} onSubmit={sendEmail}>
       <h2 className='form__title'>Contact</h2>
       <p className='form__body'>If you have a project that you need coded, please don't hesitate to contact me.</p>
+      {message && <p className='form__message'>{message}</p>}
       <Form.Group className='mb-3' controlId="formName">
         <Form.Label className='form__label'>Name</Form.Label>
         <Form.Control className='form__control' type="name" placeholder="Enter name" name='user_name' required />
