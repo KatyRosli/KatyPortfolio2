d797828eb00e6d223255a410f7d988f1518502e6
Katy Rosli <katyrosli@gmail.com>
2023-04-17 13:29:46 +0200

    remove unused code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
 export function NavBar() {
