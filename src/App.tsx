7289567ac6b904764937937ab7fe86a629624124
Katy Rosli <katyrosli@gmail.com>
2023-07-04 18:53:07 +0200

    implement GDPR

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { Home } from './pages/Home/Home';
@@ -11,11 +11,28 @@ import ReactGA from 'react-ga';
 const TRACKING_ID = "UA-263832841-1";
 ReactGA.initialize(TRACKING_ID);
 
+const COOKIE_CONSENT_KEY = 'cookieConsent';
+
 export default function App() {
+  const [cookieConsent, setCookieConsent] = useState(false);
 
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    const storedConsent = localStorage.getItem(COOKIE_CONSENT_KEY);
+    if (storedConsent === 'true') {
+      setCookieConsent(true);
+    }
   }, []);
+
+  useEffect(() => {
+    if (cookieConsent) {
+      localStorage.setItem(COOKIE_CONSENT_KEY, 'true');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, [cookieConsent]);
+
+  const handleAccept = () => {
+    setCookieConsent(true);
+  };
 
   return (
     <main id="bootstrap-overrides">
@@ -27,6 +44,12 @@ export default function App() {
         </Routes>
         <Footer />
       </Router>
+      {!cookieConsent && (
+        <div className='cookie-consent-banner'>
+          <p className='cookie-text'>This website uses cookies to enchance the user experience.</p>
+          <button className='cookie-button' onClick={handleAccept}>I understand</button>
+        </div>
+      )}
     </main>
   );
 }
